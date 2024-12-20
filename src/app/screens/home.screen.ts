4608ecebc57aa5f01b8966991ce53f0d519880fa
Ken Ovienadu <ken.ovienadu@upflex.com>
2024-12-20 05:32:22 +0100

    feat: add spaces between product cards on mobile

--- src/app/screens/home.screen.ts
+++ src/app/screens/home.screen.ts
@@ -43,7 +43,7 @@ import { CartService } from "../services/cart.service";
       </div>
 
       <!-- Product Cards -->
-      <div *ngIf="filteredProducts.length; else empty_state" class="md:grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-12" >
+      <div *ngIf="filteredProducts.length; else empty_state" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 mt-12" >
         <product-card
           *ngFor="let product of filteredProducts"
           [product]="product"
