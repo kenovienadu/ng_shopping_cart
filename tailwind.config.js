8d555b5adfbc37c7429287cdcdde786a87e1094a
Ken Ovienadu <ken.ovienadu@upflex.com>
2024-12-20 01:37:43 +0100

    feat: setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}" // Includes all Angular templates and TypeScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
